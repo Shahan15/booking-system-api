bdbc0c15bfdd84b9f7bd177a6c7576cfe48ee804
shahan15 <k.shahan786@gmail.com>
2025-08-09 16:27:44 +0100

    added POST and GET request

---
+++ helpers/arrayEmpty.js
@@ -0,0 +1,5 @@
+function isArrayEmpty(arr){
+    return Array.isArray(arr) && arr.length === 0;
+};
+
+module.exports = { isArrayEmpty };
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const {isArrayEmpty} = require('./helpers/arrayEmpty')
+
+let resourceHolder = []
+app.use(express.json())
+
+
+
+app.get('/', (req,res)=>{
+    res.send("Hello world")
+})
+
+app.post('/resources', (req,res) => {
+    const newResource = req.body
+    resourceHolder.push(newResource)
+    res.status(200).json(resourceHolder)
+});
+
+app.get('/resources',(req,res) =>{
+    if(isArrayEmpty(resourceHolder) === true){
+        res.status(400).json({message: "Array is empty, cannot fetch resource"})
+    } else {
+        res.status(200).json({message: "fetching resources...",
+            resourceHolder
+        });
+    }
+
+    console.log(resourceHolder)
+});
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000")
+});
